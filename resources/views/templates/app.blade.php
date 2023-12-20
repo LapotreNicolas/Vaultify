1fe33011db3184dca85527099b1553da28ba62c1
CoRE.ExE <98487589+CoRExE@users.noreply.github.com>
2023-12-20 21:14:19 +0100

    package-lock.json add to ignore to avoid merge conflit

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -11,7 +11,6 @@
 <header>Ma super application</header>
 <nav>
     <a href="{{route('index')}}">Accueil</a>
-    <a href="{{route('test-vite')}}">Test Vite</a>
     <a href="#">Contact</a>
 
 @auth

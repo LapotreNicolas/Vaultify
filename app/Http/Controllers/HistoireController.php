00068420d100c5c26e96966ee16fa363dd3ded20
Thomas Mouton <thomas_mouton2@ens.univ-artois.fr>
2023-12-20 15:59:19 +0100

    modif HistoireController.php

--- app/Http/Controllers/HistoireController.php
+++ app/Http/Controllers/HistoireController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Genre;
 use App\Models\Histoire;
+use http\Client\Curl\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 
 class HistoireController extends Controller
@@ -39,12 +42,52 @@ class HistoireController extends Controller
         return view('history.indexHistory',['titre' => "Liste des histoires", 'histoires' => $histoire, 'cat' => $cat, 'genreFiltres' => $genre]);
     }
 
+    public function create()
+    {
+        $genres = Genre::all();
+        return view('history.createHistory', ['genres' => $genres]);
+    }
+
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        {
+            $this->validate(
+                $request,
+                [
+                    'titre' => 'required',
+                    'pitch' => 'required',
+                    'active' => 'required',
+                    'photo' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+                    'genre_id' =>' required',
+                ]
+            );
+
+            // A partir d'ici le code est exécuté uniquement si les données sont validaées
+            // par la méthode validate()
+            // sinon un message d'erreur est renvoyé vers l'utilisateur
+
+            // préparation de l'enregistrement à stocker dans la base de données
+            $histoire = new Histoire();
+
+            $histoire->titre = $request->titre;
+            $histoire->pitch = $request->pitch;
+            $histoire->active = $request->active;
+            $photo = $request->file('photo')->store('images', 'public');
+            $histoire->photo = $photo;
+            $histoire->user_id = Auth::id();
+            $histoire->genre_id = $request->input('genre_id');
+
+            // insertion de l'enregistrement dans la base de données
+            $histoire->save();
+
+            // redirection vers la page qui affiche la liste des tâches
+            return redirect()->route('history.index')
+                ->with('type', 'primary')
+                ->with('msg', 'Scene ajoutée avec succès');
+        }
     }
 
     /**

6730145060940f588828e51cc95d6db6bc337d1b
Lou-Anne <saejinmx@gmail.com>
2023-12-21 13:36:33 +0100

    Test

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
+    <img src="{{asset("images/Village.jpg")}}" class="logimg">
     <section class="log">
-        <img src="{{asset("images/Village.jpg")}}" alt="">
         <form action="{{route("login")}}" method="post" class="card">
             @csrf
             <h3>Connexion</h3>

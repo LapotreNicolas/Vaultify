7f31862f0b537b639fb4e1a1226252f7ad2cddff
Snay77 <77ethan.barlet@gmail.com>
2023-12-21 13:23:21 +0100

    premier test sur equipe non fonctionnel

--- resources/views/equipes/index.blade.php
+++ resources/views/equipes/index.blade.php
@@ -1,23 +1,27 @@
 <x-layout>
-    <h2>Nom d'équipe : {{$equipe['nomEquipe']}}</h2>
-    <h3>Slogan : {{$equipe['slogan']}}</h3>
-    <h3>Localisation : {{$equipe['localisation']}}</h3>
-    <h3>Membres de l'équipe : </h3>
-    <ul>
-        @foreach($equipe['membres'] as $membre)
-            <li>
-                <h3>Nom : {{$membre['nom']}}</h3>
-                <h3>Prénom : {{$membre['prenom']}}</h3>
-                <img src="{{$membre['image']}}" alt="avatar">
-                <h3>Liste des fonctions : </h3>
-                <ul>
-                @foreach($membre['fonctions'] as $fonction)
-                    <li>{{$fonction}}</li>
+    <div class="container">
+        <div class="equipe">
+            <h2>Nom d'équipe : {{$equipe['nomEquipe']}}</h2>
+            <h3>Slogan : {{$equipe['slogan']}}</h3>
+            <h3>Localisation : {{$equipe['localisation']}}</h3>
+            <h3>Membres de l'équipe : </h3>
+            <ul>
+                @foreach($equipe['membres'] as $membre)
+                    <li>
+                        <h3>Nom : {{$membre['nom']}}</h3>
+                        <h3>Prénom : {{$membre['prenom']}}</h3>
+                        <img src="{{$membre['image']}}" alt="avatar">
+                        <h3>Liste des fonctions : </h3>
+                        <ul>
+                        @foreach($membre['fonctions'] as $fonction)
+                            <li>{{$fonction}}</li>
+                        @endforeach
+                        </ul>
+                    </li>
                 @endforeach
-                </ul>
-            </li>
-        @endforeach
-    </ul>
-    <h4>Autres informations : {{$equipe['autres']}}</h4>
-    <img src="{{$equipe['logo']}}" alt="logo">
+            </ul>
+            <h4>Autres informations : {{$equipe['autres']}}</h4>
+            <img src="{{$equipe['logo']}}" alt="logo">
+        </div>
+    </div>
 </x-layout>

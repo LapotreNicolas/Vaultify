aa7b3c1f870e0e8ec6f3ff886e3065a0edd55c86
Snay77 <77ethan.barlet@gmail.com>
2023-12-21 17:28:26 +0100

    dernier commit pour le équipe

--- resources/views/equipes/index.blade.php
+++ resources/views/equipes/index.blade.php
@@ -42,7 +42,7 @@
                 </ul>
                 <h4>Autres informations : {{$equipe['autres']}}</h4>
                 <img src="{{asset('storage/images/'.$equipe['logo'])}}" alt="logo">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/VIDEO_ID" frameborder="0" allowfullscreen></iframe>
+            <iframe style="margin-bottom: 75px" width="560" height="315" src="https://www.youtube.com/embed/xmXYsTUOKyQ" frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
 </x-layout>

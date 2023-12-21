71ecfea23ffc64bf195ab3bba5edc5ba7ea4e66a
Killian <killianm2259@gmail.com>
2023-12-21 14:20:50 +0100

    login + register fini

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,22 @@
 <x-layout>
-    <img src="{{asset("images/Village.jpg")}}" class="logimg">
-    <section class="log">
-        <img src="{{asset("images/Village.jpg")}}" alt="">
-        <form action="{{route("login")}}" method="post" class="card login">
-            @csrf
-            <h3>Connexion</h3>
-            <input type="email" name="email" required placeholder="Email" /><br />
-            <input type="password" name="password" required placeholder="Password" /><br />
-            Remember me<input type="checkbox" name="remember"   /><br />
-            <input type="submit" /><br />
-        </form>
-    </section>
+    <div class="logimg">
+        <img src="{{asset("images/Village.jpg")}}">
+        <div class="card login">
+            <form action="{{route("login")}}" method="post">
+                @csrf
+                <h3>Connexion</h3>
+                <input type="email" name="email" required placeholder="Email" />
+                <input type="password" name="password" required placeholder="Mot de passe" />
+                <div class="remember">
+                    <p>Se souvenir de moi</p>
+                    <input type="checkbox" name="remember"   />
+                </div>
+                <input type="submit" />
+                <div class="bottomLog">
+                    <p>Pas de compte ?</p>
+                    <a href="{{route("register")}}">Inscris-toi</a>
+                </div>
+            </form>
+        </div>
+    </div>
 </x-layout>

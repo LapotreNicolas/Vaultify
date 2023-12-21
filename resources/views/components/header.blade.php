314e15a4781c0b5fcd38eddd192ea9002373ba57
CoRE.ExE <98487589+CoRExE@users.noreply.github.com>
2023-12-21 01:37:00 +0100

    Patch header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,7 @@
         <a href="{{route('story.index')}}">Histoires</a>
         <a href="{{route('contact')}}">Contact</a>
     @auth
-        <a href="{{route('history.create')}}">Créer une histoire</a>
+        <a href="{{route('story.create')}}">Créer une histoire</a>
     @endauth
     </div>
     <a href="{{route('accueil')}}"><img src="{{asset("images/logo.svg")}}" alt="logo" class="imgHeader"></a>
@@ -14,14 +14,9 @@
         </div>
     @endguest
     @auth
-<<<<<<< HEAD
         <div class="right">
-=======
-        <div>
-            <a href="{{route('story.create')}}">Créer une histoire</a>
->>>>>>> adf5a334d9c7b60c2e9015b719a3175efb26f99a
-            {{Auth::user()->name}}
-            <a href="{{route('profil')}}">Profil</a>
+            <a href="{{route('contact')}}">Contact</a>
+            <a href="{{route('profil')}}">{{Auth::user()->name}}</a>
             <a href="{{route("logout")}}"
             onclick="document.getElementById('logout').submit(); return false;">Logout</a>
             <form id="logout" action="{{route("logout")}}" method="post">

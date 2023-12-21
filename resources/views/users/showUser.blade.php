570304ae531b6290454512301f9b2ca27080db0b
Snay77 <77ethan.barlet@gmail.com>
2023-12-21 15:49:41 +0100

    css showuser

--- resources/views/users/showUser.blade.php
+++ resources/views/users/showUser.blade.php
@@ -1,10 +1,20 @@
 <x-layout titre="Utilisateur">
+    <div class="homeImg">
+        <img src="{{asset("images/homeImg.jpg")}}" alt="homeImg">
+        <div class="homeTextImg">
+            <h1>{{$user->name}}</h1>
+            <h2>{{$user->email}}</h2>
+            <img src="../images/ornament1.svg" alt="ornament ">
+            <h3>{{$user->name}} a fini de lire {{$finies}} histoire</h3>
+        </div>
+        {{-- <a href="#homeContent"><i class='bx bx-down-arrow-alt'></i></a> --}}
+    </div>
     <div>
-        <ul>
+        {{-- <ul>
             <li>{{$user->name}}</li>
             <li>{{$user->email}}</li>
             <li>{{$finies}}</li>
-        </ul>
+        </ul> --}}
         @foreach($creees as $histoire)
             <ul>
                 <a href="{{route("story.show",$histoire->id)}}">
